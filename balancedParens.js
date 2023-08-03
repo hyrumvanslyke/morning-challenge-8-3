72c0a473277e98ebc15c66c167cb508394930268
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-08-03 10:43:53 -0400

    Added text for explanation

--- balancedParens.js
+++ balancedParens.js
@@ -5,15 +5,20 @@ let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
 // Write your solution below:
-function balancedParens(str) {
-    let open = 0
-    let closed = 0
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] === "(") {
+function balancedParens(str) {// funcion created
+    let newStr = str.split('')
+    let open = 0// start count for open "("
+    let closed = 0// start count for closed ")"
+    for (let i = 0; i < newStr.length; i++) {// loop through the string
+      if (str[i] === "(") {// add one to open if there is a (
         open += 1
-      } else if (str[i] === ")") {
+      } else if (str[i] === ")") {// add one to open if there is a )
         closed += 1
       }
     }
-    return open === closed
+    return open === closed// if they are equal return true
   }
+  console.log(balancedParens(sample1))//logged for testing
+  console.log(balancedParens(sample2))//logged for testing
+  console.log(balancedParens(sample3))//logged for testing
+  console.log(balancedParens(sample4))//logged for testing
